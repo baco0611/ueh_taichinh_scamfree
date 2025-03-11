858de7aca3c622099ba5bf3b88ed87df4a8cb3d5
baco0611 <baoblink@gmail.com>
2025-03-12 06:03:32 +0900

    [CREATE] Create handbook section an content list block

--- src/view/Handbook/Handbook.jsx
+++ src/view/Handbook/Handbook.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import "./Handbook.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import title from "./img/title.png"
+import contentList from "./img/contentList.png"
+import contentData from './data';
 
 export default function Handbook() {
     return (
-        <div>Handbook</div>
+        <div className='page-wrapper' id='handbook-section'>
+            <img className='handbook-title' src={title}/>
+
+            <div className='search-bar wrapper'>
+                <input
+                    placeholder='Tìm kiếm thông tin'
+                />
+                <button>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+
+            <div className='content-list wrapper'>
+                <img className='title' src={contentList}/>
+                <div className='content'>
+                {
+                    contentData.map((element, index) => {
+                        return (
+                        <div className='element' key={index}>
+                            <p>{element.content}</p>
+                        </div>
+                        )
+                    })
+                }
+                </div>
+            </div>
+        </div>
     )
 }
---
+++ src/view/Handbook/data.js
@@ -0,0 +1,289 @@
+const contentData = [
+    {
+    "id": 1,
+    "content": "Rao bán hàng giả, hàng nhái trên sàn thương mại điện tử",
+    "signs": [
+        "Lorem ipsum dolor sit amet",
+        "Ut labore et dolore magna aliqua"
+    ],
+    "prevent": [
+        "Velit esse cillum dolore eu fugiat"
+    ]
+    },
+    {
+        "id": 2,
+        "content": "Giả mạo trang thông tin điện tử của cơ quan, doanh nghiệp",
+        "signs": [
+            "Consectetur adipiscing elit"
+        ],
+        "prevent": [
+            "Ut enim ad minim veniam",
+            "Laboris nisi ut aliquip ex ea commodo"
+        ]
+    },
+    {
+        "id": 3,
+        "content": "Thông báo “khóa sim” vì chưa chuẩn hóa thuê bao",
+        "signs": [
+            "Duis aute irure dolor in reprehenderit",
+            "Excepteur sint occaecat cupidatat non proident",
+            "Quis nostrud exercitation ullamco"
+        ],
+        "prevent": [
+            "Duis aute irure dolor in reprehenderit",
+            "Velit esse cillum dolore eu fugiat",
+            "Nulla pariatur excepteur sint occaecat"
+        ]
+    },
+    {
+        "id": 4,
+        "content": "Lừa đảo chứng khoán, tiền ảo, đa cấp",
+        "signs": [
+            "Lorem ipsum dolor sit amet",
+            "Ut labore et dolore magna aliqua"
+        ],
+        "prevent": [
+            "Ut enim ad minim veniam"
+        ]
+    },
+    {
+        "id": 5,
+        "content": "Dịch vụ lấy lại tài khoản Facebook",
+        "signs": [
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Nulla pariatur excepteur sint occaecat"
+        ]
+    },
+    {
+        "id": 6,
+        "content": "Đánh cắp telegram OTP",
+        "signs": [
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Laboris nisi ut aliquip ex ea commodo"
+        ]
+    },
+    {
+        "id": 7,
+        "content": "Rải link Phishing, Seeding quảng cáo bẩn trên MXH",
+        "signs": [
+            "Consectetur adipiscing elit",
+            "Lorem ipsum dolor sit amet"
+        ],
+        "prevent": [
+            "Sunt in culpa qui officia deserunt"
+        ]
+    },
+    {
+        "id": 8,
+        "content": "Lừa đảo cuộc gọi video Deepfake, Deepvoice",
+        "signs": [
+            "Excepteur sint occaecat cupidatat non proident"
+        ],
+        "prevent": [
+            "Nulla pariatur excepteur sint occaecat",
+            "Mollit anim id est laborum",
+            "Sunt in culpa qui officia deserunt"
+        ]
+    },
+    {
+        "id": 9,
+        "content": "Lừa đảo SMS Brandname, phát tán tin nhắn giả mạo",
+        "signs": [
+            "Excepteur sint occaecat cupidatat non proident"
+        ],
+        "prevent": [
+            "Sunt in culpa qui officia deserunt",
+            "Laboris nisi ut aliquip ex ea commodo",
+            "Mollit anim id est laborum"
+        ]
+    },
+    {
+        "id": 10,
+        "content": "Chuyển nhầm tiền vào tài khoản ngân hàng",
+        "signs": [
+            "Quis nostrud exercitation ullamco",
+            "Lorem ipsum dolor sit amet",
+            "Consectetur adipiscing elit"
+        ],
+        "prevent": [
+            "Ut enim ad minim veniam"
+        ]
+    },
+    {
+        "id": 11,
+        "content": "Lừa đảo tuyển cộng tác viên online",
+        "signs": [
+            "Ut labore et dolore magna aliqua"
+        ],
+        "prevent": [
+            "Ut enim ad minim veniam",
+            "Sunt in culpa qui officia deserunt"
+        ]
+    },
+    {
+        "id": 12,
+        "content": "Cho số đánh đề",
+        "signs": [
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Velit esse cillum dolore eu fugiat",
+            "Duis aute irure dolor in reprehenderit"
+        ]
+    },
+    {
+        "id": 13,
+        "content": "Giả danh cơ quan công an, viện kiểm sát, tòa án gọi điện lừa đảo",
+        "signs": [
+            "Ut labore et dolore magna aliqua"
+        ],
+        "prevent": [
+            "Laboris nisi ut aliquip ex ea commodo",
+            "Sunt in culpa qui officia deserunt"
+        ]
+    },
+    {
+        "id": 14,
+        "content": "Giả danh nhân viên y tế báo người thân đang cấp cứu",
+        "signs": [
+            "Consectetur adipiscing elit",
+            "Duis aute irure dolor in reprehenderit",
+            "Lorem ipsum dolor sit amet"
+        ],
+        "prevent": [
+            "Nulla pariatur excepteur sint occaecat",
+            "Sunt in culpa qui officia deserunt",
+            "Velit esse cillum dolore eu fugiat"
+        ]
+    },
+    {
+        "id": 15,
+        "content": "Đánh cắp tài khoản MXH, nhắn tin lừa đảo",
+        "signs": [
+            "Ut labore et dolore magna aliqua",
+            "Consectetur adipiscing elit"
+        ],
+        "prevent": [
+            "Laboris nisi ut aliquip ex ea commodo",
+            "Duis aute irure dolor in reprehenderit",
+            "Ut enim ad minim veniam"
+        ]
+    },
+    {
+        "id": 16,
+        "content": "Tung tin giả về cuộc gọi mất tiền FlashAI",
+        "signs": [
+            "Sed do eiusmod tempor incididunt",
+            "Duis aute irure dolor in reprehenderit"
+        ],
+        "prevent": [
+            "Laboris nisi ut aliquip ex ea commodo",
+            "Velit esse cillum dolore eu fugiat"
+        ]
+    },
+    {
+        "id": 17,
+        "content": "Giả mạo biên lai chuyển tiền thành công",
+        "signs": [
+            "Consectetur adipiscing elit",
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Duis aute irure dolor in reprehenderit",
+            "Velit esse cillum dolore eu fugiat",
+            "Sunt in culpa qui officia deserunt"
+        ]
+    },
+    {
+        "id": 18,
+        "content": "Tuyển người mẫu nhí",
+        "signs": [
+            "Quis nostrud exercitation ullamco",
+            "Sed do eiusmod tempor incididunt",
+            "Duis aute irure dolor in reprehenderit"
+        ],
+        "prevent": [
+            "Velit esse cillum dolore eu fugiat"
+        ]
+    },
+    {
+        "id": 19,
+        "content": "Cài cắm ứng dụng, link quảng cáo cờ bạc, cá độ, tín dụng đen",
+        "signs": [
+            "Consectetur adipiscing elit",
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Ut enim ad minim veniam",
+            "Velit esse cillum dolore eu fugiat"
+        ]
+    },
+    {
+        "id": 20,
+        "content": "Bẫy tín dụng, đầu tư tài chính, gửi bưu kiện, trúng thưởng",
+        "signs": [
+            "Quis nostrud exercitation ullamco",
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Laboris nisi ut aliquip ex ea commodo",
+            "Duis aute irure dolor in reprehenderit",
+            "Nulla pariatur excepteur sint occaecat"
+        ]
+    },
+    {
+        "id": 21,
+        "content": "Thủ đoạn giả danh các công ty tài chính, ngân hàng",
+        "signs": [
+            "Excepteur sint occaecat cupidatat non proident",
+            "Quis nostrud exercitation ullamco",
+            "Ut labore et dolore magna aliqua"
+        ],
+        "prevent": [
+            "Mollit anim id est laborum",
+            "Ut enim ad minim veniam"
+        ]
+    },
+    {
+        "id": 22,
+        "content": "Đánh cắp thông tin CCCD đi vay nợ tín dụng",
+        "signs": [
+            "Consectetur adipiscing elit",
+            "Ut labore et dolore magna aliqua",
+            "Lorem ipsum dolor sit amet"
+        ],
+        "prevent": [
+            "Nulla pariatur excepteur sint occaecat",
+            "Velit esse cillum dolore eu fugiat"
+        ]
+    },
+    {
+        "id": 23,
+        "content": "Dịch vụ lấy lại tiền khi đã bị lừa",
+        "signs": [
+            "Sed do eiusmod tempor incididunt"
+        ],
+        "prevent": [
+            "Mollit anim id est laborum"
+        ]
+    },
+    {
+        "id": 24,
+        "content": "Combo du lịch giá rẻ",
+        "signs": [
+            "Excepteur sint occaecat cupidatat non proident",
+            "Ut labore et dolore magna aliqua"
+        ],
+        "prevent": [
+            "Mollit anim id est laborum",
+            "Sunt in culpa qui officia deserunt",
+            "Nulla pariatur excepteur sint occaecat"
+        ]
+    }
+]
+
+export default contentData
