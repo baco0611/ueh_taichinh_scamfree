8f6b9b54cb545d95f850e898741a4ff5b2b2734c
baco0611 <baoblink@gmail.com>
2025-03-13 18:21:27 +0900

    [CREATE] Create prevent element

--- src/view/Handbook/Handbook.jsx
+++ src/view/Handbook/Handbook.jsx
@@ -9,6 +9,7 @@ import rightSign from "./img/rightSign.png"
 import onlineExample from "./img/onlineExample.png"
 import { contentData } from "./data"
 import ContentListElement from './ContentListElement';
+import { Link } from 'react-router-dom';
 
 export default function Handbook() {
     return (
@@ -43,14 +44,70 @@ export default function Handbook() {
             <div className='sign-prevent wrapper'>
                 <img className='title' src={signPrevent}/>
                 <div className='content'>
-                
+                    <div className='element'>
+                        <h3>ĐỐI TƯỢNG MỤC TIÊU TRONG CÁC VỤ LỪA ĐẢO</h3>
+                        <p>Các hình thức lừa đảo trên không gian mạng ngày càng tinh vi và phổ biến, nhắm vào nhiều nhóm đối tượng khác nhau, bao gồm người cao tuổi, sinh viên, nhân viên văn phòng và nhiều đối tượng khác.</p>
+                        <p>Đối với <span>người cao tuổi</span>, thủ đoạn thường là giả mạo cuộc gọi từ ngân hàng hoặc cơ quan nhà nước, yêu cầu cung cấp thông tin cá nhân hoặc mã xác thực.</p>
+                        <p>Đối với <span>sinh viên</span>, các kẻ lừa đảo thường sử dụng chiêu thức khuyến mãi ảo, trò chơi trúng thưởng hoặc hứa hẹn công việc hấp dẫn để lấy thông tin và tiền bạc. </p>
+                        <p>Trong khi đó, <span>nhân viên văn phòng</span> thường là mục tiêu của các thư mạo danh từ đối tác hoặc các trò lừa đảo liên quan đến tài chính.</p>
+                        <p>&rarr; Mỗi nhóm đối tượng ở từng độ tuổi khác nhau, kẻ xấu sẽ thực hiện những phương thức dẫn dụ khác nhau, tuy nhiên mục tiêu chung là để lấy lòng tin, từ đó dễ dàng đánh cắp thông tin người dùng, cuối cùng là chiếm đoạt tài sản.</p>
+                    </div>
+                    <div className='element'>
+                        <h3>NGUYÊN TẮC AN TOÀN TRƯỚC CÁC HÌNH THỨC LỪA ĐẢO  </h3>
+                        <ul>
+                            <li>- <span>Không click</span> vào các liên kết lạ, các trang web không rõ nguồn gốc.</li>
+                            <li>- <span>Không cung cấp</span> thông tin cá nhân quan trọng như CCCD, số thẻ ngân hàng, mật khẩu, mã OTP.</li>
+                            <li>- <span>Không tin</span> vào những lời hứa hẹn về lợi nhuận cao hoặc những cơ hội làm giàu nhanh chóng.</li>
+                            <li>- <span>Không chuyển</span> tiền vào tài khoản lạ khi chưa xác minh rõ ràng.</li>
+                        </ul>
+                    </div>
+                    <div className='element'>
+                        <h3>HƯỚNG DẪN XỬ LÝ</h3>
+                        <ul>
+                            <li>- Giữ bình tĩnh và trấn an bản thân.</li>
+                            <li>- Lưu lại các bằng chứng để báo cáo cho cơ quan chức năng hỗ trợ và xử lý kịp thời.</li>
+                        </ul>
+                    </div>
+                    <div className='element'>
+                        <h3>CÁC TỔ CHỨC HỖ TRỢ KHI GẶP RẮC RỐI</h3>
+
+                        <div className='contact'>
+                            <div className='block'>
+                                <p><span>Bộ thông tin và truyền thông</span></p>
+                                <ul>
+                                    <li>- Số điện thoại: 024.3556.3461</li>
+                                    <li>- Website: <a target='_blank' href="https://mic.gov.vn/">https://mic.gov.vn/</a></li>
+                                </ul>
+                            </div>
+                            <div className='block'>
+                                <p><span>Cổng thông tin hỗ trợ người dùng</span></p>
+                                <ul>
+                                    <li>- Số điện thoại: 1900.888.655</li>
+                                    <li>- Website: <a target='_blank' href="https://ais.gov.vn/">https://ais.gov.vn/</a></li>
+                                </ul>
+                            </div>
+                            <div className='block'>
+                                <p><span>Cục An toàn thông tin (AIS)</span></p>
+                                <ul>
+                                    <li>- Số điện thoại: 024.3209.6789</li>
+                                    <li>- Website: <a target='_blank' href="https://ais.gov.vn/">https://ais.gov.vn/</a></li>
+                                </ul>
+                            </div>
+                            <div className='block'>
+                                <p><span>Cổng thông tin điện tử Bộ công an</span></p>
+                                <ul>
+                                    <li>- Số điện thoại: 069.2343647</li>
+                                    <li>- Website: <a target='_blank' href="www.bocongan.gov.vn ​​​​​">www.bocongan.gov.vn ​​​​​</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
 
             <div className='online-example wrapper'>
                 <img className='title' src={onlineExample}/>
-                <div>
-
+                <div className='content'>
                 </div>
             </div>
         </div>
