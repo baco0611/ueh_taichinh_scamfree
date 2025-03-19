2c201b0b62876d627bd3b4bea3dc8dcd3e153b8b
baco0611 <baoblink@gmail.com>
2025-03-19 21:22:19 +0900

    [CREATE] Create feedbackLayout without data

--- src/view/Feedback/Feedback.jsx
+++ src/view/Feedback/Feedback.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Feedback.scss"
+import title from "./img/title.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
+import button from "./img/button.png"
 
 export default function Feedback() {
+    const [ emailContent, setEmailContent ] = useState("")
+    console.log(emailContent)
+
     return (
-        <div>Feedback</div>
+        <div className='page-wrapper' id='feedback'>
+            <img className='title' src={title}/>
+
+            <div className='container'>
+                <div className='header'>
+                    <div></div>
+                    <div>
+                        <FontAwesomeIcon icon={faMinus}/>
+                        <FontAwesomeIcon icon={faSquare}/>
+                        <FontAwesomeIcon icon={faXmark}/>
+                    </div>
+                </div>
+                <div className='content'>
+                    <div className='main'>
+                        <textarea
+                            value={emailContent}
+                            onChange={e => setEmailContent(e.target.value)}
+                            autoFocus={true}
+                            placeholder='Chúng mình có thể làm gì để cải thiện?'
+                        />
+                        <button>
+                            <img src={button}/>
+                        </button>
+                    </div>
+                    <div className='text'>
+                        <p>Chúng mình trân trọng ý kiến đóng góp của bạn!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
