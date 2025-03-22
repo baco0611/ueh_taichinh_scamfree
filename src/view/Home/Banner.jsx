4227dcc111cb22d7f5d74525453ccdbbd8aad038
baco0611 <baoblink@gmail.com>
2025-03-22 16:43:34 +0900

    [CREATE] Create banner

--- src/view/Home/Banner.jsx
+++ src/view/Home/Banner.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import image1 from "./img/banner/1.png"
 
 export default function Banner() {
     return (
         <div id='banner'>
-            
+            <img src={image1}/>
         </div>
     )
 }
